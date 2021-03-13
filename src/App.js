cf42c42c5d6d23c301d9f20ca43cdf3bef39ffd0
HarshitMaheshwari <mr.harshit1997@gmail.com>
2021-03-13 01:02:50 +0000

    API for countries list in the dropdown implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import {
   MenuItem,
@@ -8,9 +8,26 @@ import {
 
 
 function App() {
-  const [countries, setCountries] = useState([
-    'USA', 'UK', 'INDIA'
-  ]);
+  const [countries, setCountries] = useState(['USA', 'UK', 'INDIA']);
+
+  useEffect(() => {
+
+    const getCountriesData = async() => {
+      await fetch('https://disease.sh/v3/covid-19/countries').then((response) => response.json()).then((data) => {
+        const countries = data.map((country) => (
+          {
+            name: country.country,
+            value: country.countryInfo.iso2
+
+          }));
+          setCountries(countries);
+
+      });
+    };
+    getCountriesData();
+  }, []);
+
+
   return (
     <div className="app">
       <div className='app_header'>
@@ -21,16 +38,10 @@ function App() {
           value='abc' 
         >
           {countries.map(country=>(
-            <MenuItem value={country}>{country}</MenuItem>
+            <MenuItem value={country.value}>{country.name}</MenuItem>
 
           ))}
-
-          {/* <MenuItem value='worldwide'>Worldwide</MenuItem>
-          <MenuItem value='worldwide'>OPtion two</MenuItem>
-          <MenuItem value='worldwide'>Option 3</MenuItem>
-          <MenuItem value='worldwide'>Option 4</MenuItem> */}
-
-
+          
         </Select>
 
       </FormControl>
