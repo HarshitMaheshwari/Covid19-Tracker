bc0617da669fed7b78324b64c46c41b8b86a88f6
HarshitMaheshwari <mr.harshit1997@gmail.com>
2021-03-13 00:48:10 +0000

    Header created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-
+import React, { useState } from 'react';
 import './App.css';
+import {
+  MenuItem,
+  FormControl,
+  Select,
+} from '@material-ui/core';
+
 
 function App() {
+  const [countries, setCountries] = useState([
+    'USA', 'UK', 'INDIA'
+  ]);
   return (
     <div className="app">
-      <h1>Lets build a Covid 19 Tracker</h1>
+      <div className='app_header'>
+      <h1>Covid 19 Tracker</h1>
+      <FormControl className='app_dropdown'>
+        <Select
+          variant='outlined'
+          value='abc' 
+        >
+          {countries.map(country=>(
+            <MenuItem value={country}>{country}</MenuItem>
+
+          ))}
+
+          {/* <MenuItem value='worldwide'>Worldwide</MenuItem>
+          <MenuItem value='worldwide'>OPtion two</MenuItem>
+          <MenuItem value='worldwide'>Option 3</MenuItem>
+          <MenuItem value='worldwide'>Option 4</MenuItem> */}
+
+
+        </Select>
+
+      </FormControl>
+      </div> 
       
+
+      {/* header */}
+      {/* title + select input dropdown */}
+
+      {/* infoboxes */}
+      {/* infoboxes */}
+      {/* infoboxes */}
+
+      {/* table */}
+      {/* graph */}
+
+      {/* map */}
     </div>
   );
 }
